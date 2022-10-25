b20003937b7cbf9c7c54c5b8dc81ea4f53d1605f
SubidShrestha <subideminem@gmail.com>
2022-10-25 23:59:20 +0545

    Initial Commit

---
+++ ecommerce/stores/models.py
@@ -0,0 +1,56 @@
+from users.models import User
+from django.db import models
+
+class Product(models.Model):
+    name=models.CharField(max_length=26,null=False)
+    CATEGORIES=[('Mens Wear','Mens Wear'),
+                ('Womens Wear','Womens Wear'),
+                ('Electronics','Electronics'),
+                ('Home Appliances','Home Appliances'),
+                ('Kids Wear','Kids Wear'),
+                ('Decors & Gardening','Decors & Gardening'),
+                ('Accessories','Accessories')]
+    category=models.CharField(max_length=26,choices = CATEGORIES,null=False)
+    image = models.ImageField(upload_to='uploads/products',null=True)
+    price=models.IntegerField()
+    date_added=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    status=models.BooleanField(default=False)
+    date_added=models.DateField(auto_now=True)
+
+    @property
+    def cart_total(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.total for item in cartitems])
+        return total
+    
+    @property
+    def cart_items(self):
+        cartitems = self.cartitem_set.all()
+        total = sum([item.quantity for item in cartitems])
+        return total
+    
+    
+    
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE,null=False)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE,null=False)
+    quantity=models.IntegerField(default=1)
+
+    @property
+    def total(self):
+        amount = self.product.price * self.quantity
+        return amount
+
+class ShippingLocation(models.Model):
+    city=models.CharField(max_length=26,null=False)
+    state=models.CharField(max_length=26,null=False)
+    location=models.CharField(max_length=26,null=False)
+    delivery_status=models.BooleanField(default=False)
+    date_added=models.DateField(auto_now_add=True)
+    
